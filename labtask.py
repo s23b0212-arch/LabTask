f640e0038dd67846d92d59506c18d25642f7f634
s23b0212-arch <s23b0212@siswa.umk.edu.my>
2026-01-14 20:41:37 +0800

    Update labtask.py

--- labtask.py
+++ labtask.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+np.random.seed(42)
 # --------------------------
 # Page Configuration
 # --------------------------
